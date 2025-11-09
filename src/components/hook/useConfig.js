760dce483b55af336d3ae7e962ff0f27150bf2ff
djidji90 <machimboleoncio@gmail.com>
2025-11-09 11:40:50 +0100

    bien hecho jordi

--- src/components/hook/useConfig.js
+++ src/components/hook/useConfig.js
@@ -1,14 +1,12 @@
+import { config } from "../../config";
+
 export const useConfig = () => {
   return {
     api: {
-      baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000",
-      tiendaNumber: import.meta.env.VITE_TIENDA_NUMERO || 1,
+      baseURL: config.API_URL,
+      tiendaNumber: config.TIENDA_NUMERO
     },
-    contact: {
-      email: import.meta.env.VITE_CONTACT_EMAIL || "",
-    },
-    social: {
-      instagram: import.meta.env.VITE_SOCIAL_INSTAGRAM || "",
-    }
+    contact: config.CONTACT,
+    social: config.SOCIAL
   };
 };
